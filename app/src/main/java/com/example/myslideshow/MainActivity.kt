e3e8bf2feb4f257053e4ad0d4e5c571aec681c87
nao18 <justdo.nao01@gmail.com>
2021-05-03 13:11:46 +0900

    ページ切り替えにアニメーションを追加

--- app/src/main/java/com/example/myslideshow/MainActivity.kt
+++ app/src/main/java/com/example/myslideshow/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.myslideshow
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.myslideshow.databinding.ActivityMainBinding
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var player: MediaPlayer
+
     class MyAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
 
         private val resources = listOf(
@@ -34,11 +40,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.pager.adapter = MyAdapter(this)
+        binding.pager.setPageTransformer(ViewPager2PageTransformation())
+
         val handler = Handler(Looper.getMainLooper())
         timer(period = 5000) {
             handler.post {
                 binding.apply {
                     pager.currentItem = (pager.currentItem + 1) % 10
+                }
+            }
+        }
+        player = MediaPlayer.create(this, R.raw.getdown)
+        player.isLooping = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        player.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        player.pause()
+    }
+
+    class ViewPager2PageTransformation : ViewPager2.PageTransformer {
+        override fun transformPage(page: View, position: Float) {
+
+            when {
+                position < -1 -> {
+                    page.alpha = 0.2f
+                    page.scaleX = 0.2f
+                    page.scaleY = 0.2f
+                }
+                position <= 1 -> {
+                    page.alpha = Math.max(0.2f, 1 - Math.abs(position))
+                    page.scaleX = Math.max(0.2f, 1 - Math.abs(position))
+                    page.scaleY = Math.max(0.2f, 1 - Math.abs(position))
+                }
+                else -> {
+                    page.alpha = 0.2f
+                    page.scaleX = 0.2f
+                    page.scaleY = 0.2f
                 }
             }
         }
